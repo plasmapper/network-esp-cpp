ca82810fef1a856a3bce528370c6e8aaaab6a6b4
plasmapper <99073058+plasmapper@users.noreply.github.com>
2026-08-04 16:19:14 +0200

    Changed lock timeout handling

--- component/pl_tcp_server.cpp
+++ component/pl_tcp_server.cpp
@@ -33,12 +33,9 @@ TcpServer::~TcpServer() {
 
 esp_err_t TcpServer::Lock(TickType_t timeout) {
   esp_err_t error = mutex.Lock(timeout);
-  if (error == ESP_OK)
-    return ESP_OK;
-  if (error == ESP_ERR_TIMEOUT && timeout == 0)
-    return ESP_ERR_TIMEOUT;
-  ESP_RETURN_ON_ERROR(error, TAG, "mutex lock failed");
-  return ESP_OK;
+  if (error != ESP_OK && (error != ESP_ERR_TIMEOUT || timeout != 0))
+    ESP_LOGE(TAG, "mutex lock failed");
+  return error;
 }
 
 //==============================================================================
